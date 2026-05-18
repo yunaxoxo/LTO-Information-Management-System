2548db48f16a5750e5173b4e1d2b31d1e4b96a83
Rein <reinmalonzo28@gmail.com>
2026-05-18 08:44:33 +0800

    feat: added pooling feature in database.py

--- src/database.py
+++ src/database.py
@@ -13,3 +13,20 @@ DB_CONFIG = {
     'password': 'root', #change this to your own password
     'database': 'lto_system'
 } 
+
+#Create a connection pool 
+@st.cache_resource
+def get_connection_pool():
+    try: 
+        pool = pooling.MySQLConnectionPool(pool_name="lto_pool", pool_size=5, **DB_CONFIG)
+        return pool
+    except Exception as e:
+        st.error(f"Error creating connection pool: {e}")
+        return None
+
+def get_connection():
+    pool = get_connection_pool()
+    if pool:
+       return pool.get_connection()
+    else:
+        return None
